db8875616965fd36019e239cba1816ce2fb1598d
Kyle Adams <kyleadams3000@hotmail.com>
2021-05-04 11:23:52 -0500

    reogranized and added shadows

---
+++ Assets/Scripts/Components/Tilemap/TilemapShadow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+using System.Linq;
+
+public class TilemapShadow : MonoBehaviour
+{
+    [SerializeField]
+    protected Tilemap _tileMap;
+    [SerializeField]
+    protected Tilemap _shadowMap;
+    [SerializeField]
+    protected Tile _shadow;
+
+    private void Awake()
+    {
+        Tilemap.tilemapTileChanged += OnTileMapChanged;
+    }
+
+    private void OnDestroy()
+    {
+        Tilemap.tilemapTileChanged -= OnTileMapChanged;
+    }
+    protected void OnTileMapChanged(Tilemap tilemap, Tilemap.SyncTile[] tiles)
+    {
+        if (tilemap != _tileMap) { return; }
+        tiles.ToList().ForEach(delta => _shadowMap.SetTile(delta.position, delta.tile ? _shadow : null));
+    }
+}
